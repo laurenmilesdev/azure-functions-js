93f8baa632fcedd1980ace72bdaac102a863ba4e
Lauren <laurenmiles.dev@gmail.com>
2023-06-17 09:01:36 -0400

    Updated helpers.js and added unit tests

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,9 +1,7 @@
-import { COMPLETED_GAME_STATUS, WIN_RESULT } from '../constants/rapidapi.js';
 import MLB_TEAMS from '../constants/mlb-teams.js';
 
 export default class Helpers {
-  getCurrentDateString() {
-    const date = new Date();
+  getFormattedDateString(date) {
     let month = date.getMonth() + 1;
     let day = date.getDate();
 
@@ -11,56 +9,6 @@ export default class Helpers {
     if (day < 10) day = `0${day}`;
 
     return `${date.getFullYear()}${month}${day}`;
-  }
-
-  checkForStats(body, team) {
-    let stats;
-    const games = Object.keys(body);
-
-    games.forEach((game) => {
-      if (game.toUpperCase().includes(team.toUpperCase())) {
-        stats = body[game];
-      }
-    });
-
-    return stats;
-  }
-
-  formatStats(stats) {
-    const { gameStatus } = stats;
-    const { home } = stats;
-    const { away } = stats;
-    const formattedStats = {
-      homeTeam: home,
-      awayTeam: away,
-      score: {
-        [home]: stats.lineScore.home.R,
-        [away]: stats.lineScore.away.R,
-      },
-      hits: {
-        [home]: stats.lineScore.home.H,
-        [away]: stats.lineScore.away.H,
-      },
-      gameStatus,
-      currentInning: stats.currentInning,
-      scoresByInning: {
-        [home]: stats.lineScore.home.scoresByInning,
-        [away]: stats.lineScore.away.scoresByInning,
-      },
-      errors: {
-        [home]: stats.lineScore.home.E,
-        [away]: stats.lineScore.away.E,
-      },
-    };
-
-    if (gameStatus === COMPLETED_GAME_STATUS) {
-      const homeWin = stats.homeResult === WIN_RESULT;
-
-      formattedStats.win = homeWin ? home : away;
-      formattedStats.loss = homeWin ? away : home;
-    }
-
-    return formattedStats;
   }
 
   getTeamNameFromAbbr(abbr) {
---
+++ tests/helpers/helpers.test.js
@@ -0,0 +1,24 @@
+import Helpers from '../../helpers/helpers.js';
+
+describe('Helpers', () => {
+  const helpers = new Helpers();
+
+  describe('getFormattedDateString', () => {
+    it('returns formatted date string', () => {
+      const date = new Date('2023-06-16T03:24:00');
+      const response = helpers.getFormattedDateString(date);
+
+      expect(response).toEqual('20230616');
+    });
+  });
+
+  describe('getTeamNameFromAbbr', () => {
+    it('returns team name from abbreviation', () => {
+      const abbr = 'DET';
+      const expectedRespone = 'Detroit Tigers';
+      const response = helpers.getTeamNameFromAbbr(abbr);
+
+      expect(response).toEqual(expectedRespone);
+    });
+  });
+});
