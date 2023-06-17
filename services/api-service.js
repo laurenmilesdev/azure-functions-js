43bf043de1a9c431cf6479f402bdc51338bc148e
Lauren <laurenmiles.dev@gmail.com>
2023-06-16 23:25:34 -0400

    Updated api-service

--- services/api-service.js
+++ services/api-service.js
@@ -2,18 +2,34 @@ import axios from 'axios';
 
 export default class ApiService {
   async get(url, config) {
-    return await axios.get(url, config);
+    try {
+      return await axios.get(url, config);
+    } catch (error) {
+      return error;
+    }
   }
 
   async post(url, data, config) {
-    return await axios.post(url, data, config);
+    try {
+      return await axios.post(url, data, config);
+    } catch (error) {
+      return error;
+    }
   }
 
   async put(url, data, config) {
-    return await axios.put(url, data, config);
+    try {
+      return await axios.put(url, data, config);
+    } catch (error) {
+      return error;
+    }
   }
 
   async delete(url, config) {
-    return await axios.delete(url, config);
+    try {
+      return await axios.delete(url, config);
+    } catch (error) {
+      return error;
+    }
   }
 }
